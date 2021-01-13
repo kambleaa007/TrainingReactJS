39a5f93b4f5d741bf2040c1be0923f3b509fcaa4
kambleaa007 <ashishkamble191191@gmail.com>
2021-01-13 19:20:20 +0530

    task-9-added

--- src/Component/FutureDatedPayment.js
+++ src/Component/FutureDatedPayment.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState, useContext } from 'react';
 import Header from "./Header";
+import { Table } from "react-bootstrap";
 
-//import './SendMoney.css'
+import './FutureDatedPayment.css'
 
 import { GlobalContext } from "../Context/GlobalContext";
 
@@ -20,11 +21,64 @@ function FutureDatedPayment(props) {
 
 
 function FutureDatedPaymentBody(props) {
+
+    
+    const globalContext = useContext(GlobalContext);
+    let transactions = globalContext.Global.Accounts[0].transactions // take transactions arr for account 1
+
+    
+
     return(
-        <div>
-FutureDatedPaymentBody
+        <div className="Send">
+            <div className="d-flex justify-content-around">
+                
+                <Table striped borderless hover className="payment-table ">
+                    <thead>
+                        <tr>
+                        {/* <th>#</th> */}
+                        <th>Status</th>
+                        <th>Payee Name</th>
+                        <th>Amount</th>
+                        <th>Due Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {   
+                        transactions.map(i=>{
+                            return(                                
+                                <tr key={i.id}>
+                                    {/* <td>{i.id}</td> */}
+                                    <td className={getStyleStatus(i.status)}>
+                                        {i.status}
+                                    </td>
+                                    <td>{i.payee_name}</td>
+                                    <td className="text-center">{i.amount}</td>
+                                    <td>{i.due_date}</td>
+                                </tr>                                
+                            )
+                        })
+                    }
+                    </tbody>
+                </Table>
+                
+                
+            </div>
         </div>
     )
+}
+
+
+// working of this function depends on JSON transaction status data should match
+function getStyleStatus(status) {
+    if(status==="PENDING")
+        return "payment-pending-status"
+    else if(status==="UNSUCCESSFUL")
+        return "payment-unsuccess-status"    
+    else if(status==="SUCCESSFUL")
+        return "payment-success-status"  
+    else if(status==="CANCELLED")
+        return "payment-cancel-status"
+    return ""          
 }
 
 export default FutureDatedPayment;
