36156510545c88dd538559f7c4dbb58fb2e9afe1
kambleaa007 <ashishkamble191191@gmail.com>
2021-03-24 15:03:35 +0530

    ...

--- src/Layout/MyAppLayout.js
+++ src/Layout/MyAppLayout.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState }from 'react';
+import { BrowserRouter, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
 
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Switch as Switch1 } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -16,29 +17,45 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 
+
 function MyAppLayout(){
+
+    const history = useHistory();
+    let { path, url } = useRouteMatch();
 
     const[collapsed, setCollapsed] = useState(false);
     var onCollapse = collapsed => {
         console.log(collapsed);
         setCollapsed(collapsed);
     };
+    const[theme, setTheme] = useState('dark');
+    var changeTheme = value => {
+        value ? setTheme('dark') : setTheme('light');
+  };
 
     return(
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{ minHeight: '100vh' }} >
             <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
+            <Switch1
+                checked = { theme === "dark" }
+                onChange = {changeTheme}
+                checkedChildren= "Dark"
+                unCheckedChildren= "Light"
+                />
+            <br />
+            <br />
+            <Menu theme = {theme} defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<PieChartOutlined />} onClick={()=>{history.push(`${url}/option1`)}}>
                 Option 1
                 </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                <Menu.Item key="2" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/option2`)}}>
                 Option 2
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined /> } title="User" >
+                <Menu.Item key="3" onClick={()=>{history.push(`${url}/user/tom`)}}>Tom</Menu.Item>
+                <Menu.Item key="4" onClick={()=>{history.push(`${url}/user/bill`)}}>Bill</Menu.Item>
+                <Menu.Item key="5" onClick={()=>{history.push(`${url}/user/alex`)}}>Alex</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
                 <Menu.Item key="6">Team 1</Menu.Item>
@@ -57,7 +74,44 @@ function MyAppLayout(){
                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
                 <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                Bill is a cat.
+                <Switch>
+                    <Route exact path={path}>
+                    </Route>
+                    <Route path={`${path}/option1`}>
+                        <div>
+                        Option1      
+                        </div>
+                    </Route>
+                    <Route path={`${path}/option2`}>
+                        <div>
+                        Option2      
+                        </div>
+                    </Route>
+                    <Route path={`${path}/user`}>
+                        <div>
+                            <h3>Option3 Users</h3>
+                            <Switch>
+                                <Route exact path={path}>
+                                </Route>
+                                <Route path={`${path}/user/tom`}>
+                                    <div>
+                                        Hello Tom
+                                    </div>
+                                </Route>
+                                <Route path={`${path}/user/bill`}>
+                                    <div>
+                                        Hello Bill
+                                    </div>
+                                </Route>
+                                <Route path={`${path}/user/alex`}>
+                                    <div>
+                                        Hello Alex
+                                    </div>
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Route>
+                </Switch>
                 </div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>Ant Design Layout Footer</Footer>
