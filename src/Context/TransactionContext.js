5bd49ced9f215e9fccdbe954c93c89fad52846aa
kambleaa007 <ashishkamble191191@gmail.com>
2021-03-25 15:27:35 +0530

    axios added...

--- src/Context/TransactionContext.js
+++ src/Context/TransactionContext.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 
 
 const TransactionContext = React.createContext();
@@ -139,6 +140,23 @@ function TransactionProvider(props) {
     );
 
 }
+
+
+
+
+export const getAccounts = async dispatch => {
+
+
+    await 
+
+}
+
+
+
+
+
+
+
 
 let TransactionConsumer = TransactionContext.Consumer;
 
