a4e9aa303bcd62cc5d409c079ee22c108ff37550
kambleaa007 <ashishkamble191191@gmail.com>
2021-03-25 10:15:11 +0530

    ...

--- src/Layout/MyAppLayout.js
+++ src/Layout/MyAppLayout.js
@@ -36,47 +36,47 @@ function MyAppLayout(){
     return(
         <Layout style={{ minHeight: '100vh' }} >
             <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} className={theme==='dark'? "ant-layout-sider-dark": "ant-layout-sider-light"}>
-            <div className="logo" />
-            <Switch1  checked = { theme === "dark" } onChange = {changeTheme} checkedChildren= "Dark" unCheckedChildren= "Light"  />
-            <br />
-            <br />
-            <Menu theme = {theme} defaultSelectedKeys={['1']} mode="inline">
-                
-                <Menu.Item key="1" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/option1`)}}>                  
-                {/* <Menu.Item key="1" icon={<PieChartOutlined />} onClick={()=>{history.push(`${url}/`+siderLayoutArray[0])}}>  */}
-                {/* {siderJson[siderLayoutArray[0]]} */}
-                Option 1
-                </Menu.Item>
-                {/* <Menu.Item key="2" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/option2`)}}> */}
-                <Menu.Item key="2" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/`+siderLayoutArray[1])}}>
-                Option 2
-                </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined /> } title="User" >
-                <Menu.Item key="3" onClick={()=>{history.push(`${url}/user/tom`)}}>Tom</Menu.Item>
-                <Menu.Item key="4" onClick={()=>{history.push(`${url}/user/bill`)}}>Bill</Menu.Item>
-                <Menu.Item key="5" onClick={()=>{history.push(`${url}/user/alex`)}}>Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
-                Files
-                </Menu.Item>
-            </Menu>
+                <div className="logo" />
+                <Switch1  checked = { theme === "dark" } onChange = {changeTheme} checkedChildren= "Dark" unCheckedChildren= "Light"  />
+                <br />
+                <br />
+                <Menu theme = {theme} defaultSelectedKeys={['1']} mode="inline">
+                    
+                    <Menu.Item key="1" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/option1`)}}>                  
+                    {/* <Menu.Item key="1" icon={<PieChartOutlined />} onClick={()=>{history.push(`${url}/`+siderLayoutArray[0])}}>  */}
+                    {/* {siderJson[siderLayoutArray[0]]} */}
+                    Option 1
+                    </Menu.Item>
+                    {/* <Menu.Item key="2" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/option2`)}}> */}
+                    <Menu.Item key="2" icon={<DesktopOutlined />} onClick={()=>{history.push(`${url}/`+siderLayoutArray[1])}}>
+                    Option 2
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<UserOutlined /> } title="User" >
+                    <Menu.Item key="3" onClick={()=>{history.push(`${url}/user/tom`)}}>Tom</Menu.Item>
+                    <Menu.Item key="4" onClick={()=>{history.push(`${url}/user/bill`)}}>Bill</Menu.Item>
+                    <Menu.Item key="5" onClick={()=>{history.push(`${url}/user/alex`)}}>Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9" icon={<FileOutlined />}>
+                    Files
+                    </Menu.Item>
+                </Menu>
             </Sider>
             <Layout className="site-layout">
             <Header className="site-layout-background" style={{ padding: 0 }} />
             <Content style={{ margin: '0 16px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item href="/">
-                    <HomeOutlined />
-                </Breadcrumb.Item>
-                
-                <Breadcrumb.Item><a href="/myAppLayout">MyAppLayout</a></Breadcrumb.Item> 
-                <Breadcrumb.Item><a href="/myAppLayout/user">User</a></Breadcrumb.Item> 
-                <Breadcrumb.Item><a href="/myAppLayout/user/bill">Bill</a></Breadcrumb.Item>
-                <Breadcrumb.Item><a href="/myAppLayout/option1">Option1</a></Breadcrumb.Item>
+                    <Breadcrumb.Item href="/">
+                        <HomeOutlined /> 
+                    </Breadcrumb.Item>
+                    
+                    <Breadcrumb.Item><a href="/myAppLayout">MyAppLayout</a></Breadcrumb.Item> 
+                    <Breadcrumb.Item><a href="/myAppLayout/user">User</a></Breadcrumb.Item> 
+                    <Breadcrumb.Item><a href="/myAppLayout/user/bill">Bill</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a href="/myAppLayout/option1">Option1</a></Breadcrumb.Item>
                 </Breadcrumb>
                 <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
                 <Switch>
