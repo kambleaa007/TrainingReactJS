7769d2df2509e8819dce73b9018c7f26a5fe680e
kambleaa007 <ashishkamble191191@gmail.com>
2021-05-05 16:36:15 +0530

    ...

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+
+
+// import { Configure } from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
+
+// Configure({ adaptor : new Adapter() });
