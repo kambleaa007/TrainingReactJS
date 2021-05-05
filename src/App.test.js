e9bc1f2c5c384c6ae63a160fd761f37b04b3903e
kambleaa007 <ashishkamble191191@gmail.com>
2021-05-05 19:24:02 +0530

    ...

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import HomeComponent from './Layout/Home';
 
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -9,12 +10,16 @@ configure({ adapter: new Adapter() });
 
 describe("Test1", () => {
 
-  test('renders learn react link', () => {
+  test('renders div app text', () => {
   
     const wrapper = shallow(<App />)
     //console.log(wrapper.debug())
     expect(wrapper.find("div").text()).toContain("div App")
   });
 
+  test('render li text', () => {
+    const wrapper = shallow(<HomeComponent />)
+    expect(wrapper.find("li").text()).toContain("Send Money or pay a bill")
+  })
 
 })
