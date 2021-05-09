af7735b405c0ec0e3bf85b72ad953c94ef0c1522
kambleaa007 <ashishkamble191191@gmail.com>
2021-05-09 11:29:11 +0530

    ...

--- src/Common/NavBar.js
+++ src/Common/NavBar.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
 
-export default function NavBar() {
+export default function NavBar(props) {
+
     return (
         <div>
             <React.Fragment>                
                 <Nav 
                     id="nav"
                     activeKey="/home"
-                    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                    //onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}                    
                     >NavBar
                     <Nav.Item>
-                        <Nav.Link id="navlink-1" href="/home">Active</Nav.Link>
+                        <Nav.Link id="navlink-1" onClick = {props.navHome}>DPMA</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-1">Link</Nav.Link>
+                        <Nav.Link id="navlink-2" onClick = {props.navAdmin}>Admin</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-2">Link</Nav.Link>
+                        <Nav.Link >Link</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-3">Disabled</Nav.Link>
+                        <Nav.Link >Disabled</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </React.Fragment>
